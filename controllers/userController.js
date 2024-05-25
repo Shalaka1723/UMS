93deabdd00eefc6386bc6ff46885d3605b05f8dc
Shalaka <shalakaw17@gmail.com>
2024-05-25 22:40:51 +0530

    basic

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,6 +11,23 @@ const loadRegister = async(req,res)=>{
 
 const insertUser = async(req,res)=>{
     try{
+        const user = new User({
+            name:req.body.name,
+            email:req.body.email,
+            mobile:req.body.mno,
+            image:req.file.filename,
+            password:req.body.password,
+            is_admin:0,
+        });
+
+        const userData = await user.save();
+
+        if (userData) {
+            res.render('registration',{message: 'Registration successful!'})
+        }
+        else{
+            res.render('registration',{message: 'Registration Failed!'})
+        }
 
     }catch(error){
         console.log(error.message);
@@ -18,5 +35,6 @@ const insertUser = async(req,res)=>{
 }
 
 module.exports = {
-    loadRegister
+    loadRegister,
+    insertUser
 }
